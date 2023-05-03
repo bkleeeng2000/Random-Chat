8c7aeea89f240ce1a28c88750e0d2b386f69a99c
이채호 <lch0412@clonix.co.kr>
2023-05-03 21:09:20 +0900

    Add : Chats Init
    - module
    - gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsModule } from './chats/chats.module';
 import mongoose from 'mongoose';
 import * as process from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URI)
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ChatsModule
   ],
   controllers: [AppController],
   providers: []
