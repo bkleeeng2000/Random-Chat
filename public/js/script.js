a0179465090708e07d083d0a21514f23cf542f12
LCH <bkleeeng2000@naver.com>
2023-05-10 22:35:18 +0900

    Add : New User
    - user 추가 시 user_connected broadcast 메시지 발생하는 부분 추가

--- public/js/script.js
+++ public/js/script.js
@@ -5,13 +5,37 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+const drawHelloStranger = (userName) => {
+  helloStrangerElement.innerHTML = `Hello ${userName} Stranger`;
+};
+
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  wrapperChatBox.innerHTML = `<div>${message}</div>`;
+  chattingBoxElement.append(wrapperChatBox);
+};
+
+// GlobalSocket Event
+socket.on('user_connected', (userName) => console.log(`${userName} connected`));
+
+// event callback function
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue === '') return;
+
+  socket.emit('submit_chat', inputValue);
+  drawNewChat(inputValue);
+};
+
 function helloUser() {
   const userName = prompt('What is your name?');
-  // helloStrangerElement.innerHTML = `Hello ${userName}`;
+  socket.emit('new_user', userName, (data) => drawHelloStranger(data));
 }
 
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
